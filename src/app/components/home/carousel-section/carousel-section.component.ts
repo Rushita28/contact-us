3b70bf59a5b69bd61d124e5fac1f9a113a76d233
tanvi-pawale <tpawale@mittalsolutionsllc.com>
2018-08-10 01:38:50 -0500

    few components of home page

---
+++ src/app/components/home/carousel-section/carousel-section.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-section',
+  templateUrl: './carousel-section.component.html',
+  styleUrls: ['./carousel-section.component.css']
+})
+
+
+export class CarouselSectionComponent implements OnInit {
+
+  constructor() {
+	
+  }
+
+  ngOnInit() {
+  }
+
+}
